aed94f68f66932f57f0fc90d8f0a22083aded73a
Hrushikesh <141734736+hrushikeshv7@users.noreply.github.com>
2026-08-22 22:53:52 +0530

    Fixed bugs

--- attacksim/sim1.py
+++ attacksim/sim1.py
@@ -4,7 +4,7 @@ import time
 TARGET = "10.145.74.155"
 IFACE  = "wlan0"
 
-print("🔴 Simulating PORT SCAN...")
+print(" Simulating PORT SCAN...")
 for port in range(1, 200):
     pkt = IP(dst=TARGET, ttl=30) / TCP(dport=port, flags="S", sport=RandShort())
     send(pkt, iface=IFACE, verbose=False)
@@ -12,14 +12,14 @@ for port in range(1, 200):
         print(f"   Scanned {port} ports...")
 time.sleep(2)
 
-print("🔴 Simulating DDoS FLOOD...")
+print(" Simulating DDoS FLOOD...")
 pkts = [IP(dst=TARGET, ttl=10) / TCP(dport=80, flags="S", sport=RandShort())
         for _ in range(300)]
 for p in pkts:
     send(p, iface=IFACE, verbose=False)
 time.sleep(2)
 
-print("🔴 Simulating BRUTE FORCE SSH...")
+print(" Simulating BRUTE FORCE SSH...")
 for i in range(100):
     pkt = IP(dst=TARGET, ttl=60) / TCP(dport=22, flags="S", sport=RandShort())
     send(pkt, iface=IFACE, verbose=False)
